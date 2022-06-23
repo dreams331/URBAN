25de2050bbeb62655e7cab706e63f60f1c8c1422
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-23 20:02:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,21 @@ provider "google" {
   region = var.region
 }
 
+terraform {
+  backend "gcs" {
+    bucket = "urban-tf-state-staging"
+    prefix = terraform/state
+  }
+}  
+    
+required_providers {
+  google = {
+    source = hashicorp/google"
+    version = ~> 4.0"
+  }
+}  
+
+
 resource "google_compute_network" "default" {
   name                    = var.network_name
   auto_create_subnetworks = false
