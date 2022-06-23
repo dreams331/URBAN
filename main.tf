e67722f38dde41cfe2992b7f7246fb31e60a267f
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-23 16:11:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-variable "region" {
-  default = "europe-west2"
-}
-
-variable "location" {
-  default = "europe-west2-a"
-}
-
-variable "network_name" {
-  default = "tf-gke-k8s"
-}
-
-provider "google" {
-  region = var.region
-}
-
-resource "google_compute_network" "default" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
 
 resource "google_compute_subnetwork" "default" {
   name                     = var.network_name
