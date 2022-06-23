1b2e18857d2f6c28ca13309a668f9d8de0442069
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-23 20:00:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,28 @@
+variable "region" {
+  default = "us-west1"
+}
+variable "project_id" {
+  default = "eletu-354214"
+}
+
+variable "location" {
+  default = "us-west1-b"
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+provider "google" {
+  region = var.region
+}
+
 resource "google_compute_network" "default" {
   name                    = var.network_name
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "private" {
+resource "google_compute_subnetwork" "default" {
   name                     = var.network_name
   ip_cidr_range            = "10.127.0.0/20"
   network                  = google_compute_network.default.self_link
@@ -24,15 +43,8 @@ resource "google_container_cluster" "default" {
   initial_node_count = 3
   min_master_version = data.google_container_engine_versions.default.latest_master_version
   network            = google_compute_subnetwork.default.name
-  subnetwork         = google_compute_subnetwork.private.name
-
-  // Use legacy ABAC until these issues are resolved: 
-  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
-  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+  subnetwork         = google_compute_subnetwork.default.name
   enable_legacy_abac = true
-
-  // Wait for the GCE LB controller to cleanup the resources.
-  // Wait for the GCE LB controller to cleanup the resources.
   provisioner "local-exec" {
     when    = destroy
     command = "sleep 90"
@@ -44,7 +56,7 @@ output "network" {
 }
 
 output "subnetwork_name" {
-  value = google_compute_subnetwork.private.name
+  value = google_compute_subnetwork.default.name
 }
 
 output "cluster_name" {
@@ -58,3 +70,4 @@ output "cluster_region" {
 output "cluster_location" {
   value = google_container_cluster.default.location
 }
+
