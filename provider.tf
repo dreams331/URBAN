b164f0506e99f45eb8f66027f6615353775861b6
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-23 16:23:07 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,35 @@
-provider "google" {
-  project     = "my-urban-project"
-  region      = "europe-west2"
+variable "region" {
+  default = "europe-west2"
 }
+
+variable "location" {
+  default = "europe-west2-a"
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+provider "google" {
+  region = var.region
+}
+
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "urban-tf-state-staging"
+    prefix = terraform/state
+  }
+}  
+    
+required_providers {
+  google = {
+    source = hashicorp/google"
+    version = ~> 4.0"
+  }
+}  
